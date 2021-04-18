81b6a93b158b375f2426814edda3a732008af05b
zcx <zcx_demail@163.com>
2021-04-18 10:29:43 +0800

    01

---
+++ 01-数据操作/01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Time    : 2021/4/16 15:09
+    Author  : 春晓
+    Software: PyCharm
+"""
+import torch
+
+x = torch.arange(12)
+# tensor([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11])
+
+
